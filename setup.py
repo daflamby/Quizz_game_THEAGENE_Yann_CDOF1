7218ded5978b88d3a1f927424dae7defe785f0e3
daflamby <157586163+daflamby@users.noreply.github.com>
2025-01-16 11:51:55 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='Quizz_game_THEAGENE_Yann_CDOF1',
+ version='1.0',
+ author='daflamby
+ license='MIT',
+ long_description=open('README.md').read(),
+)
